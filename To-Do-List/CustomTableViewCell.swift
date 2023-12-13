2b868a6edf666a1d78a0065ec461ef4b99c78e86
Omar Pakr <omarpakr74@gmail.com>
2023-12-13 12:31:27 +0200

    check if tableView empty

---
+++ To-Do-List/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  To-Do-List
+//
+//  Created by Omar Pakr on 12/12/2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var toDoLabel : UILabel!
+
+}
--- To-Do-List/ViewController.swift
+++ To-Do-List/ViewController.swift
@@ -15,13 +15,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       initTools()
+        
+      func isTableViewEmpty() -> Bool {
+            return tableView.numberOfSections == 0 || tableView.numberOfRows(inSection: 0) == 0
+        }
+        
+        if isTableViewEmpty() {
+            
+            let imageError = UIImageView(frame: CGRect(x: 50, y: 100, width:
+            self.view.frame.width - 100, height: 200))
+            imageError.image = UIImage(systemName: "clear")
+            imageError.tintColor = UIColor(red: 69/255, green: 230/255, blue: 72/255, alpha: 1)
+            self.view.addSubview(imageError)
+            let labelMsg = UILabel(frame: CGRect(x: imageError.frame.minX, y:
+            imageError.frame.maxY + 15, width: imageError.frame.width, height:
+            30))
+            labelMsg.text =
+            "Add new item"
+            labelMsg.textAlignment = .center
+            self.view.addSubview(labelMsg)
+    
+            }
+        
+        
        
+    }
+    
+    private func initTools(){
         tableView.delegate = self
         tableView.dataSource = self
         
         if let savedItems = UserDefaults.standard.stringArray(forKey: "todoItems") {
-                    items = savedItems
-                }
+            items = savedItems
+        }
     }
 
     @IBAction func AddButton(_ sender: UIBarButtonItem) {
@@ -61,28 +88,61 @@ class ViewController: UIViewController {
 
 
 extension ViewController : UITableViewDelegate , UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
+   
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoListCell", for: indexPath) as! CustomTableViewCell
+        cell.toDoLabel.text = items[indexPath.row]
         
-        cell.textLabel?.text = items[indexPath.row]
+        
               return cell
     }
     
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
+   
+    
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) {  _,_,completion in
+            self.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            self.saveItemsToUserDefaults()
+            completion(true)
+        }
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = .systemRed
+        
+        
+        let congig = UISwipeActionsConfiguration(actions: [deleteAction])
+        return congig
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        tableView.beginUpdates()
-        items.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
-        tableView.endUpdates()
-         
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let sendActin = UIContextualAction(style: .destructive, title: nil) { _, _, completion in
+            print("Send a message to \(self.items[indexPath.row])")
+            completion(true)
+            
+        }
+        sendActin.image = UIImage(systemName: "envelope")
+        sendActin.backgroundColor = .systemMint
+        
+        let congig = UISwipeActionsConfiguration(actions: [sendActin])
+        return congig
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
     
 }
+
