8ffc51797b6ad9ba38c9b519799051c99de247ce
Omar Pakr <omarpakr74@gmail.com>
2023-12-14 16:15:09 +0200

    ...

--- To-Do-List/ViewController.swift
+++ To-Do-List/ViewController.swift
@@ -8,70 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var items = [String]()
-    
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var emptyView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initTools()
-       
-        
-        
     }
-    
-   
-    
     private func initTools(){
         tableView.delegate = self
         tableView.dataSource = self
-        
         if let savedItems = UserDefaults.standard.stringArray(forKey: "todoItems") {
             items = savedItems
         }
     }
-    
     @IBAction func AddButton(_ sender: UIBarButtonItem) {
-        
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add New item", message: "", preferredStyle: .alert)
-        
         let cancel = UIAlertAction(title: "Cancel", style: .default) { (cancel) in
-            
         }
-        
         let save = UIAlertAction(title: "Save", style: .default) { (save) in
-            
             self.items.append(textField.text!)
             self.saveItemsToUserDefaults()
             self.tableView.reloadData()
-            
-            
-            
-            
         }
         alert.addTextField { (text) in
             textField = text
             textField.placeholder = "Add New item"
         }
-        
         alert.addAction(cancel)
         alert.addAction(save)
         self.present(alert, animated: true, completion: nil)
-        
     }
     private func saveItemsToUserDefaults() {
         UserDefaults.standard.set(items, forKey: "todoItems")
     }
-    
 }
-
-
-
 extension ViewController : UITableViewDelegate , UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -85,21 +58,12 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource {
             return items.count
         }
     }
-    
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoListCell", for: indexPath) as! CustomTableViewCell
         cell.toDoLabel.text = items[indexPath.row]
-        
-        
         return cell
     }
-    
-    
-    
-    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         let deleteAction = UIContextualAction(style: .destructive, title: nil) {  _,_,completion in
@@ -110,32 +74,21 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource {
         }
         deleteAction.image = UIImage(systemName: "trash")
         deleteAction.backgroundColor = .systemRed
-        
-        
         let congig = UISwipeActionsConfiguration(actions: [deleteAction])
         return congig
     }
-    
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let sendActin = UIContextualAction(style: .destructive, title: nil) { _, _, completion in
             print("Send a message to \(self.items[indexPath.row])")
             completion(true)
-            
         }
         sendActin.image = UIImage(systemName: "envelope")
         sendActin.backgroundColor = .systemMint
-        
         let congig = UISwipeActionsConfiguration(actions: [sendActin])
         return congig
-        
     }
-    
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    
-    
 }
 
